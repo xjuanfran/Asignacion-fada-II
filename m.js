3b44bd485e7697d98af7b12bef20de5ade1bd663
xjuanFran <juan.garces.leon@correounivalle.edu.co>
2023-04-27 19:38:00 -0500

    ok 2

--- m.js
+++ m.js
@@ -1,39 +1,25 @@
-function count(x, exponente, baseAux = 1, baseAux2 = 1, cont = 0, cont2 = 2) {
-  if (x == 0) {
+function count(x, exponente) {
+  // Caso base: si x es 0, la suma es válida
+  if (x === 0) {
     return 0;
   }
 
-  sumaOne = 1 + Math.pow(cont, exponente);
-  cont++;
-  console.log(sumaOne)
-
-
-  sumaEqual = Math.pow(baseAux, exponente) + Math.pow(baseAux2, exponente);
-
-  //   if (sumaEqual > x) {
-  //     return 0;
-  //   }
-
-  //console.log(sumaEqual);
-
-  sumaDifBase = Math.pow(baseAux, exponente) + Math.pow(cont2, exponente);
-  //console.log(cont2);
-
-  //   if (sumaDifBase > x) {
-  //     return 0;
-  //   }
-
-  //console.log(sumaDifBase);
-
-  if (baseAux > x || baseAux2 > x) {
-    return 0;
+  // Caso base: si el exponente es 0, solo se puede sumar 1
+  if (exponente === 0 || exponente === 1) {
+    return x === 0;
   }
 
-  if (sumaEqual == x || sumaDifBase == x || sumaOne == x) {
-    return 1;
-  } else {
-    return count(x, exponente, baseAux + 1, baseAux2 + 1, cont,cont2+1);
+  // Buscar el primer número de la combinación
+  let num = 1;
+  while (Math.pow(num, exponente) <= x) {
+    // Buscar recursivamente los siguientes números que sumados al primer número dan x
+    if (count(x - Math.pow(num, exponente), exponente - 1)) {
+      return 1;
+    }
+    num++;
   }
+
+  // Si no se ha encontrado ninguna combinación, la suma no es válida
+  return 0;
 }
-
-console.log(count(29, 2));
+console.log(count(18, 3)); // true
