15ffadf93230013ba8aeecd595aa696ca05a6219
nico858 <91767917+nico858@users.noreply.github.com>
2023-05-03 21:44:15 -0500

    Update superDigit
    Donde how it's expected.

--- superDigit.py
+++ superDigit.py
@@ -1,20 +1,27 @@
-def superdigit(num):
+def superDigit(num):
     num_string = str(num)
     suma = 0
     if len(num_string) == 1:
-        suma += int(num_string[0])
-        return suma
+        return num
     else:
-        for i in num_string:
-            digits = len(num_string)
-            return suma + int(i) + superdigit(num % (10 ** (digits - 1)))
+        return superDigit(sumDigits(num))
+        
+
+def sumDigits(num):
+    num_string = str(num)
+    if len(num_string) == 1:
+        return int(num_string[0])
+    else:
+        digits = len(num_string)
+        return int(num_string[0]) + sumDigits(num % (10 ** (digits - 1)))  
 
 
 
 if __name__ == '__main__':
-    print(superdigit(9875))
-    print(superdigit(29))
-    print(superdigit(11))
-    print(superdigit(2))
+    print(sumDigits(9875))
+    print(sumDigits(29))
+    print(sumDigits(11))
+    print(sumDigits(2)) 
+    print(f'SuperDigit: {superDigit(9875)}') 
 
 # Temporal Complexity: O(n)
